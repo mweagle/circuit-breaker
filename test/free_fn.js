43a8e79b66c8bea689e49a7f5b55b421364486e1
Matt Weagle <mweagle@adobe.com>
2012-12-31 20:57:38 -0800

    Initial commit

---
+++ test/free_fn.js
@@ -0,0 +1,192 @@
+// Copyright (c) 2012 Matt Weagle (mweagle@gmail.com)
+
+// Permission is hereby granted, free of charge, to
+// any person obtaining a copy of this software and
+// associated documentation files (the "Software"),
+// to deal in the Software without restriction,
+// including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission
+// notice shall be included in all copies or substantial
+// portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE
+var assert = require('assert');
+var circuit_breaker = require('../lib/circuit-breaker');
+
+var SAMPLE_COUNT = 20;
+var CALL_FREQUENCY_MS = 50;
+
+describe('Single function Circuit Breakers', function() {
+  this.timeout(200000);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // SETUP
+  //
+  before(function() {
+
+  });
+
+  after(function() {
+
+  });
+
+  beforeEach(function() {
+
+  });
+
+  afterEach(function (done) {
+    done();
+  });
+
+  /////////////////////////////////////////////////////////////////////////////
+  // TESTS
+  //
+  it ('MUST handle well-behaved functions', function(done) {
+    var sample_count = SAMPLE_COUNT;
+    var call_interval = null;
+
+    var test_callback = function(error, results)
+    {
+      assert(error === null, 'Error MUST be null');
+      assert(results === null, 'Results MUST be null');
+      if (sample_count <= 0)
+      {
+        clearInterval(call_interval);
+        done();
+      }
+    };
+
+    var wrapped_function = function(callback)
+    {
+      process.nextTick(function () {
+        callback(null, null);
+      });
+    };
+    // Repeatedly call the callback
+    var gated_function = circuit_breaker.new_circuit_breaker(wrapped_function,
+                                                              0,
+                                                              10,
+                                                              10);
+    call_interval = setInterval(function () {
+      sample_count -= 1;
+      gated_function(test_callback);
+    },
+    CALL_FREQUENCY_MS);
+  });
+
+  it ('MUST handle well-behaved functions that accept arguments', function(done) {
+    var sample_count = SAMPLE_COUNT;
+    var call_interval = null;
+    var PASS_THROUGH_VALUE = 'hello world';
+
+    var test_callback = function(error, results)
+    {
+      assert(error === null, 'Error MUST be null');
+      assert(results === PASS_THROUGH_VALUE, 'Arguments MUST be propagated');
+      if (sample_count <= 0)
+      {
+        clearInterval(call_interval);
+        done();
+      }
+    };
+    var wrapped_function = function(some_value, some_value2, callback)
+    {
+      assert(some_value === PASS_THROUGH_VALUE, 'Arguments MUST be propagated');
+      process.nextTick(function () {
+        callback(null, some_value2);
+      });
+    };
+    // Repeatedly call the callback
+    var gated_function = circuit_breaker.new_circuit_breaker(wrapped_function,
+                                                              0,
+                                                              10,
+                                                              10);
+    call_interval = setInterval(function () {
+      sample_count -= 1;
+      gated_function(PASS_THROUGH_VALUE, PASS_THROUGH_VALUE, test_callback);
+    },
+    CALL_FREQUENCY_MS);
+  });
+
+  it ('MUST handle functions that fail', function(done) {
+    var sample_count = SAMPLE_COUNT;
+    var call_interval = null;
+    var error_count = 0;
+    var test_callback = function(error)
+    {
+      error_count += (error ? 1 : 0);
+      // Exit condition
+      if (sample_count <= 0)
+      {
+        // Should get non-zero errors
+        clearInterval(call_interval);
+        assert(error_count > 0, 'Should be non-zero errors due to failing functions.');
+        done();
+      }
+    };
+
+    var wrapped_function = function(callback)
+    {
+      var error = (sample_count >= 5 && sample_count <= 15) ? "Expected failure" : null;
+      process.nextTick(function () {
+        callback(error, null);
+      });
+    };
+    // Repeatedly call the callback
+    var gated_function = circuit_breaker.new_circuit_breaker(wrapped_function,
+                                                              0,
+                                                              100,
+                                                              100);
+    call_interval = setInterval(function () {
+      sample_count -= 1;
+      gated_function(test_callback);
+    },
+    CALL_FREQUENCY_MS);
+  });
+
+  it ('MUST handle functions that timeout', function(done) {
+    var sample_count = SAMPLE_COUNT;
+    var call_interval = null;
+    var error_count = 0;
+    var test_callback = function(error)
+    {
+      error_count += (error ? 1 : 0);
+    };
+
+    var wrapped_function = function(/* callback */)
+    {
+      // Just don't call anything, ever...
+      // Exit condition
+      if (sample_count <= 0)
+      {
+        // Should get non-zero errors
+        clearInterval(call_interval);
+        assert(error_count > 0, 'Should be non-zero errors due to timeouts.');
+        done();
+      }
+    };
+    // Repeatedly call the callback
+    var gated_function = circuit_breaker.new_circuit_breaker(wrapped_function,
+                                                              0,
+                                                              50,
+                                                              75);
+    call_interval = setInterval(function () {
+      sample_count -= 1;
+      gated_function(test_callback);
+    },
+    CALL_FREQUENCY_MS);
+  });
+});
---
+++ test/object_fn.js
@@ -0,0 +1,146 @@
+// Copyright (c) 2012 Matt Weagle (mweagle@gmail.com)
+
+// Permission is hereby granted, free of charge, to
+// any person obtaining a copy of this software and
+// associated documentation files (the "Software"),
+// to deal in the Software without restriction,
+// including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission
+// notice shall be included in all copies or substantial
+// portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE
+var assert = require('assert');
+var circuit_breaker = require('../lib/circuit-breaker');
+
+var SAMPLE_COUNT = 20;
+var CALL_FREQUENCY_MS = 50;
+
+var ObjectBreaker = function(fn_error_provider)
+{
+  // Add a property that must be ignored by the CircuitBreaker
+  // because it's not a function.
+  this.ignore_property = true;
+
+  var error_provider = fn_error_provider || function () {return null;};
+  Object.defineProperty(this, 'error_provider', {value: error_provider});
+
+  var self = this;
+  ['one', 'two', 'three'].forEach(function (eachName) {
+    self[eachName] = function(callback)
+    {
+      assert(this === self, 'This pointer MUST be preserved!');
+      process.nextTick(function () {
+        callback(self.error_provider(), null);
+      });
+    };
+  });
+};
+var wrapped_object = null;
+
+describe('Object-based Circuit Breakers', function() {
+  this.timeout(200000);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // SETUP
+  //
+  before(function() {
+
+  });
+
+  after(function() {
+
+  });
+
+  beforeEach(function() {
+    wrapped_object = new ObjectBreaker();
+  });
+
+  afterEach(function (done) {
+    done();
+  });
+
+  /////////////////////////////////////////////////////////////////////////////
+  // TESTS
+  //
+  it ('MUST handle object-based circuit breakers', function(done) {
+    var sample_count = SAMPLE_COUNT;
+    var call_interval = null;
+
+    var test_callback = function(error, results)
+    {
+      assert(error === null, 'Error MUST be null');
+      assert(results === null, 'Results MUST be null');
+      if (sample_count <= 0)
+      {
+        clearInterval(call_interval);
+        done();
+      }
+    };
+
+    // Repeatedly call the callback
+    var fn_names = Object.keys(wrapped_object).filter(function (eachKey) {
+      return (typeof(wrapped_object[eachKey]) === 'function');
+    });
+    var gated_object = circuit_breaker.new_circuit_breaker(wrapped_object,
+                                                              0,
+                                                              10,
+                                                              10);
+
+    call_interval = setInterval(function () {
+      sample_count -= 1;
+      var fn_call_idx = (sample_count % fn_names.length);
+      gated_object[fn_names[fn_call_idx]](test_callback);
+    },
+    CALL_FREQUENCY_MS);
+  });
+
+  it ('MUST share failure state across object functions', function(done) {
+    var sample_count = SAMPLE_COUNT;
+    var call_interval = null;
+    var error_count = 0;
+    var test_callback = function(error /*, results*/)
+    {
+      error_count += (error ? 1 : 0);
+      if (sample_count <= 0)
+      {
+        clearInterval(call_interval);
+        assert(error_count === SAMPLE_COUNT/2, "Error count should be shared");
+        done();
+      }
+    };
+    var error_provider = function() {
+      return ((sample_count % 2) === 0);
+    };
+    wrapped_object = new ObjectBreaker(error_provider);
+
+    // Repeatedly call the callback
+    var fn_names = Object.keys(wrapped_object).filter(function (eachKey) {
+      return (typeof(wrapped_object[eachKey]) === 'function');
+    });
+    var gated_object = circuit_breaker.new_circuit_breaker(wrapped_object,
+                                                              0,
+                                                              10,
+                                                              10);
+
+    call_interval = setInterval(function () {
+      sample_count -= 1;
+      var fn_call_idx = (sample_count % fn_names.length);
+      gated_object[fn_names[fn_call_idx]](test_callback);
+    },
+    CALL_FREQUENCY_MS);
+  });
+});
